b94f6fb5013a39dea675114dba8de1543a314274
Firat-Celik <56916843+Firat-Celik@users.noreply.github.com>
2020-08-10 17:03:21 +0300

    Add files via upload
    Projenin kaynak kodları ve T-SQL dosyası eklerde mevcuttur. İndirip inceleyebilirsiniz.

---
+++ TSQL.sql
@@ -0,0 +1,28 @@
+create database PersonelKayit›slemleri
+go 
+use PersonelKayit›slemleri
+go 
+create table Kisiler
+(
+ID int identity(1,1) primary key,
+Isim nvarchar(30),
+Soyisim nvarchar(30),
+EmailAdres nvarchar(60),
+TelefonNumarasi nvarchar(20),
+Resim varbinary(max)
+)
+go
+create proc KisiEkle
+(
+@Isim nvarchar(30),
+@Soyisim nvarchar(30),
+@EmailAdres nvarchar(60),
+@TelefonNumarasi nvarchar(20),
+@Resim varbinary(max)
+)
+as
+begin
+insert into Kisiler (Isim,Soyisim,EmailAdres,TelefonNumarasi,Resim)
+values (@Isim,@Soyisim,@EmailAdres,@TelefonNumarasi,@Resim)
+end
+
